a23d19b0dd4377607efa6b7420c6b1cf4c4b7839
GoryMoon <gurreja@gmail.com>
2014-10-09 21:03:02 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#SwipeToRefresh Translucent StatusBar
+
+This is a template ready to build an app on that requires AppCompat v4 SwipeToRefresh and want's to have a translucent statusbar
+
+Tested on a Nexus 5, Android 4.4.4
+
+
+![Screenshot](http://i.imgur.com/kscoG1G.png =300)
+![Animated](http://i.imgur.com/EfNOiaY.gif =300)
---
+++ app/src/main/java/se/gorymoon/swipetorefreshtemplate/MainActivity.java
@@ -0,0 +1,56 @@
+package se.gorymoon.swipetorefreshtemplate;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends ActionBarActivity implements SwipeRefreshLayout.OnRefreshListener {
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
+        swipeRefreshLayout.setOnRefreshListener(this);
+        swipeRefreshLayout.setColorSchemeResources(R.color.blue_dark, R.color.red_dark, R.color.green_light, R.color.purple_light);
+        swipeRefreshLayout.canChildScrollUp();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+
+        //getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onRefresh() {
+        //Temporary to make the animation to run, refresh data here
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                swipeRefreshLayout.setRefreshing(false);
+            }
+        }, 5000);
+    }
+}
